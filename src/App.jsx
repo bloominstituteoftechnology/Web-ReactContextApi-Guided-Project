f630d05a6f0e71f3aeef69ed6a2aa6a130e8eb24
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-10-06 16:44:30 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,33 @@ import countContext from './contexts/countContext';
 import { StyledWidget } from './styled';
 
 const initialCount = { number: 0 };
-const userData = { users: [], currentUser: null };
+const initialUserData = {
+  users: [
+    { id: '1', name: 'Pere' },
+    { id: '2', name: 'Megan' },
+    { id: '3', name: 'James' },
+  ],
+  favUser: '1',
+};
 
 function Top() {
   const [count, setCount] = useState(initialCount);
   const increment = () => setCount({ number: count.number + 1 });
 
+  const [userData, setUserData] = useState(initialUserData);
+  const setFavUser = id => setUserData({ ...userData, favUser: id });
+
   return (
     <StyledWidget color='grey'>
-      <button onClick={increment}>increment</button>
+      <div>
+        This is the top level component. The current count is {count.number}&nbsp;
+        and my favorite user is {userData.users.find(usr => usr.id === userData.favUser).name}
+      </div>
+
+      <button onClick={increment}>increment count</button><br />
+      <button onClick={() => setFavUser('1')}>fav Pere</button>
+      <button onClick={() => setFavUser('2')}>fav Megan</button>
+      <button onClick={() => setFavUser('3')}>fav James</button>
 
       <countContext.Provider value={count}>
         <Counter />
